7ee001b2890bfaa66f160d611b7bf83b9c1a5371
munificent-forum-1695 <bunchshawn@gmail.com>
2020-10-07 22:24:07 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,5 +1,5 @@
 require 'pry'
-require_relative '../fix_using_tests/the_final_frontier'# don't forget to add: require 'pry'
+# don't forget to add: require 'pry'
 
 def generate_star_date
     (rand(100000) + 400000) / 10.0
